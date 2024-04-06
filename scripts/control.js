3d289674d8c0ee15b8471e624cf744feba7f694f
rinuya <uya4coding@gmail.com>
2024-04-06 11:29:13 +0300

    added platform

--- scripts/control.js
+++ scripts/control.js
@@ -12,7 +12,9 @@ document.getElementById('right-arrow').addEventListener('click', logRight);
 document.addEventListener('keydown', function(event) {
     if (event.key === 'a') {
         logLeft();
+        movePlatform(-1);
     } else if (event.key === 'd') {
         logRight();
+        movePlatform(1);
     }
 });
--- scripts/platform.js
+++ scripts/platform.js
@@ -1,7 +1,5 @@
-
-
-var options = {
-    isStatic: true, 
+var platform_options = {
+    isStatic: false, 
     friction: 0.5,
     render: {
         fillStyle: 'white', // Set the fill color of the rectangle
@@ -9,17 +7,27 @@ var options = {
     }
 };
 // Create a moving platform
-var platform = Bodies.rectangle(window.innerWidth / 2, window.innerHeight - 150, 300, 20, options);
+var platform = Bodies.rectangle(window.innerWidth / 2, window.innerHeight - 150, 300, 70, platform_options);
 
-// Add the platform to the world
-World.add(engine.world, [platform]);
+var ground_options = {
+    isStatic: true, 
+    friction: 0.5,
+    render: {
+        fillStyle: 'black', // Set the fill color of the rectangle
+        // You can also customize other rendering properties here, such as strokeStyle, lineWidth, etc.
+    }
+};
+var ground = Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 200, ground_options);
 
 // Define movement controls for the platform
-var platformSpeed = 5;
+var platformSpeed = 10;
+
+
+World.add(engine.world, [platform, ground]);
 
 function movePlatform(direction) {
     var velocityX = direction * platformSpeed;
-    Body.setVelocity(platform, { x: velocityX, y: 0 });
+    Matter.Body.setVelocity(platform, { x: velocityX, y: 0 });
 }
 
 // Keyboard event listeners
