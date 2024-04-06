708cfce5f5d663814d9d8dd7ab7ef5973dd8e42d
rinuya <uya4coding@gmail.com>
2024-04-06 11:42:06 +0300

    d

--- scripts/control.js
+++ scripts/control.js
@@ -1,20 +1,10 @@
-function logLeft() {
-    console.log("left");
-}
-
-function logRight() {
-    console.log("right");
-}
-
-document.getElementById('left-arrow').addEventListener('click', logLeft);
-document.getElementById('right-arrow').addEventListener('click', logRight);
+document.getElementById('left-arrow').addEventListener('click', movePlatform(-1));
+document.getElementById('right-arrow').addEventListener('click', movePlatform(1));
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'a') {
-        logLeft();
         movePlatform(-1);
     } else if (event.key === 'd') {
-        logRight();
         movePlatform(1);
     }
 });
